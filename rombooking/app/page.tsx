b11658015d1c903d6c6d64da61a9b85ce233c679
ErikAndreasKlokk <eaklokk@gmail.com>
2023-09-26 13:25:03 +0200

    ja

--- rombooking/app/page.tsx
+++ rombooking/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
       <header>
-        
+        <div>
+          <Link href="">
+            <Image src="/IM logo.png" alt='Logo' width={500} height={500}></Image>
+          </Link>
+        </div>
+        <div>
+
+        </div>
       </header>
       <main>
 
