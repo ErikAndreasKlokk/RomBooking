e316dcf5133b219d1fbfd82b23ae5b53a4f5cdfd
ErikAndreasKlokk <eaklokk@gmail.com>
2023-09-27 12:34:29 +0200

    mhm

--- rombooking/app/page.tsx
+++ rombooking/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import Link from 'next/link';
+import React, { useState } from 'react';
+
+
 
 export default function Home() {
   return (
     <div className="maxwidth">
       <header>
-        <div>
+        <div className="headerdiv">
           <Link href="">
             <Image 
               src="/IM logo.png" 
@@ -14,10 +17,28 @@ export default function Home() {
               height={70}>
             </Image>
           </Link>
+          <h1>
+            Rom Booking IM
+          </h1>
+          <div className="pxboks">
+
+          </div>
         </div>
+        <hr />
       </header>
       <main>
-        
+        <div className="klasserom">
+          <button>Blå base</button>
+          <button>Esport</button>
+          <button>Lilla base</button>
+          <button>Oransj base</button>
+          <button>Grønn base</button>
+          <button>Amfiet</button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
       </main>
       <footer>
 
